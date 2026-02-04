b5af56727236c7548dee100ad6c083ad9a975120
Soumya <soumyadiphaldar3@gmail.com>
2026-02-05 00:17:49 +0530

    add edit profile route

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- server/src/routers/user.route.js
+++ server/src/routers/user.route.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const verifyUser = require("../middlewares/user.middleware.js");
+const uploader = require("../middlewares/multer.middleware.js");
 const {
   registerUser,
   loggedInUser,
   logOutUser,
   refreshAccessToken,
   changePassword,
+  editProfile
 } = require("../controllers/user.controller.js");
 
 router.post("/register", registerUser);
@@ -14,5 +16,6 @@ router.post("/login", loggedInUser);
 router.post("/logout", verifyUser, logOutUser);
 router.post("/refresh-access-token", refreshAccessToken);
 router.put("/change-password", verifyUser, changePassword);
+router.put("/edit-profile", verifyUser, uploader.single("avatar"), editProfile);
 
 module.exports = router;
